d683ea0d9155675f3bd1783116cb904f99bb56fe
Aniket singh <71018641+Aniketsuraj@users.noreply.github.com>
2024-05-21 18:04:57 +0530

    celery

--- dags/fetchDataFromMongo.py
+++ dags/fetchDataFromMongo.py
@@ -1,12 +1,20 @@
 from datetime import datetime, timedelta
 from threading import Thread
 from airflow.models import DAG
+from pymongo import MongoClient
 from airflow.operators.empty import EmptyOperator
 from airflow.operators.python import PythonOperator
 from airflow.utils.task_group import TaskGroup
+from airflow.operators.python import PythonOperator
+from datetime import datetime, timedelta
+from concurrent.futures import ThreadPoolExecutor
+from airflow import DAG
 
 import datatransformation
 from utils.mongodbHelper import get_circle_data, get_sensor_data, get_process_sensor
+
+client = MongoClient("mongodb://13.127.57.185:27017/")
+db = client.pvvnl
 
 default_args = {
     'owner': 'Shubham Bhatt',
@@ -16,27 +24,66 @@ default_args = {
 }
 
 
-def process_sensor_wrapper(sensor_info, transformed_data):
-    sen_id = sensor_info['id']
-    site_id = sensor_info['site_id']
-    processed_data = get_process_sensor(sen_id)
-    if processed_data is None or len(processed_data) < 3000:
-        return []
-    else:
-        tdata = transformed_data.append(datatransformation.init_transformation(processed_data, site_id))
-        return tdata
+# def process_sensor_wrapper(sensor_info, transformed_data):
+#     sen_id = sensor_info['id']
+#     site_id = sensor_info['site_id']
+#     processed_data = get_process_sensor(sen_id)
+#     if processed_data is None or len(processed_data) < 3000:
+#         return []
+#     else:
+#         tdata = transformed_data.append(datatransformation.init_transformation(processed_data, site_id))
+#         return tdata
 
 
-def fetch_and_transform_sensor_data(sensor_data):
-    threads = []
-    transformed_data = []
-    for sensor_info in sensor_data:
-        thread = Thread(target=process_sensor_wrapper, args=(sensor_info, transformed_data))
-        thread.start()
-        threads.append(thread)
-    for thread in threads:
-        thread.join()
-    return "success"
+# def fetch_and_transform_sensor_data(sensor_data):
+#     threads = []
+#     transformed_data = []
+#     for sensor_info in sensor_data:
+#         thread = Thread(target=process_sensor_wrapper, args=(sensor_info, transformed_data))
+#         thread.start()
+#         threads.append(thread)
+#     for thread in threads:
+#         thread.join()
+#     return "success"
+def fetch_sensor_data(sensor_id):  
+    from_id = sensor_id + "-2024-01-01 00:00:00"
+    to_id = sensor_id + "-2024-03-31 23:59:59"
+    results = list(db.load_profile_jdvvnl.find({"_id": {"$gte": from_id, "$lt": to_id}}))
+
+    for doc in results:
+        doc['_id'] = str(doc['_id'])
+        
+    
+    return results
+def get_circle_ids():
+    return [doc["id"] for doc in db.circle.find({}, {"_id": 0, "id": 1})]
+def fetch_data_for_circle(circle_id):
+    sensor_info = db.jdvvnlSensor.find(
+        {
+            "circle_id": circle_id,
+            "type": "AC_METER",
+            "admin_status": {"$in": ["N", "S", "U"]},
+            "utility": "2",
+        },
+        {"id": 1, "_id": 0},
+    )
+
+    with ThreadPoolExecutor() as executor:
+        futures = [
+            executor.submit(
+                fetch_sensor_data, doc["id"])
+            for doc in sensor_info
+        ]
+
+        for future in futures:
+            try:
+                sensor_info = future.result()
+                print(f"Fetched data for sensor: {len(sensor_info)}")
+                
+            except Exception as e:
+                print(f"Error fetching data for sensor: {e}")
+        return sensor_info
+
 
 
 with DAG(
@@ -46,20 +93,24 @@ with DAG(
         schedule_interval=None,
         max_active_runs=100
 ) as dag:
-    start = EmptyOperator(task_id="START")
-    end = EmptyOperator(task_id="END")
+    # start = EmptyOperator(task_id="START")
+    # end = EmptyOperator(task_id="END")
 
-    circle_data = get_circle_data()
+    circle_data = get_circle_ids()
 
     with TaskGroup("ParallelSensorTasks") as parallel_group:
         for circle_id in circle_data:
-            sensor_data = get_sensor_data(circle_id['id'])
-            fetch_sensor_task = PythonOperator(
-                task_id=f"FetchSensor_{circle_id['id']}",
-                python_callable=fetch_and_transform_sensor_data,
-                op_args=[sensor_data]
+            # sensor_data = get_sensor_data(circle_id['id'])
+            task = PythonOperator(
+                task_id=f"task_{circle_id}",
+                python_callable=fetch_data_for_circle,
+                provide_context=True,
+                op_kwargs={
+                    "circle_id": circle_id,
+                },
             )
-            fetch_sensor_task >> end
+            parallel_group
+    #         fetch_sensor_task >> end
 
-    start >> parallel_group
+    # start >> parallel_group
 
