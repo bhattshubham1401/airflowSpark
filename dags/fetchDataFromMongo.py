7692c6a88eefcae5bd5d613577720ad0415fe0b4
Aniket singh <71018641+Aniketsuraj@users.noreply.github.com>
2024-05-21 17:28:20 +0530

    backup

--- dags/fetchDataFromMongo.py
+++ dags/fetchDataFromMongo.py
@@ -4,44 +4,38 @@ from airflow.models import DAG
 from airflow.operators.empty import EmptyOperator
 from airflow.operators.python import PythonOperator
 from airflow.utils.task_group import TaskGroup
-import requests
-from config.config import get_database
 
 import datatransformation
 from utils.mongodbHelper import get_circle_data, get_sensor_data, get_process_sensor
 
 default_args = {
-    'owner': 'Shubham Bhatt1232222',
+    'owner': 'Shubham Bhatt',
     'start_date': datetime(2024, 4, 11),
     'retries': 3,
     'retry_delay': timedelta(seconds=120)
 }
 
 
-# def process_sensor_wrapper(sensor_info, transformed_data, circle_id):
-#     sen_id = sensor_info['id']
-#     site_id = sensor_info['site_id']
-#     processed_data = get_process_sensor(sen_id)
-#     if processed_data is None or len(processed_data) < 3000:
-#         return []
-#     else:
-#         tdata = transformed_data.append(datatransformation.init_transformation(processed_data, site_id, circle_id))
-#         return tdata
+def process_sensor_wrapper(sensor_info, transformed_data):
+    sen_id = sensor_info['id']
+    site_id = sensor_info['site_id']
+    processed_data = get_process_sensor(sen_id)
+    if processed_data is None or len(processed_data) < 3000:
+        return []
+    else:
+        tdata = transformed_data.append(datatransformation.init_transformation(processed_data, site_id))
+        return tdata
 
 
-def fetch_and_transform_sensor_data(circle_id):
-    
-    # threads = []
-    with get_database() as conn:
-        requests.get("http://13.127.57.185:5000/fetch_data_for_sensors",params={"circle_id":circle_id,"conn":conn})
-    # transformed_data = data.text
-    
-    # for sensor_info in sensor_data:
-    #     thread = Thread(target=process_sensor_wrapper, args=(sensor_info, transformed_data, circle_id))
-    #     thread.start()
-    #     threads.append(thread)
-    # for thread in threads:
-    #     thread.join()
+def fetch_and_transform_sensor_data(sensor_data):
+    threads = []
+    transformed_data = []
+    for sensor_info in sensor_data:
+        thread = Thread(target=process_sensor_wrapper, args=(sensor_info, transformed_data))
+        thread.start()
+        threads.append(thread)
+    for thread in threads:
+        thread.join()
     return "success"
 
 
@@ -63,8 +57,9 @@ with DAG(
             fetch_sensor_task = PythonOperator(
                 task_id=f"FetchSensor_{circle_id['id']}",
                 python_callable=fetch_and_transform_sensor_data,
-                op_args=[sensor_data, circle_id['id']]
+                op_args=[sensor_data]
             )
             fetch_sensor_task >> end
 
     start >> parallel_group
+
