80b0604df1ee059aa6ae258b09d0c06edb00d2b0
bhattshubham1401 <shubhambhatt687@gmail.com>
2025-03-24 17:49:12 +0530

    add

--- dags/fetchDataFromMongo.py
+++ dags/fetchDataFromMongo.py
@@ -1,8 +1,12 @@
+import logging
 from datetime import datetime, timedelta
 from airflow.models import DAG
 from airflow.operators.python import PythonOperator
 from pymongo.mongo_client import MongoClient
 from concurrent.futures import ThreadPoolExecutor, as_completed
+
+# Set up logging
+log = logging.getLogger(__name__)
 
 # MongoDB Connection
 client = MongoClient("mongodb://35.154.221.2:27017/", maxPoolSize=50)
@@ -19,6 +23,8 @@ default_args = {
 def fetch_sensor_data(sensor_id):
     from_id = sensor_id + ":2024-07-02 00:00:00"
     to_id = sensor_id + ":2025-03-18 23:59:59"
+    log.info(f"Fetching data for sensor: {sensor_id}")
+
     cursor = db.loadprofile.find({"_id": {"$gte": from_id, "$lt": to_id}}, no_cursor_timeout=True)
     for doc in cursor:
         doc['_id'] = str(doc['_id'])
@@ -27,21 +33,21 @@ def fetch_sensor_data(sensor_id):
 
 def process_sensor_data(doc):
     # Placeholder function to process each record
-    print(f"Processing data for sensor: {doc['_id']}")
+    log.info(f"Processing data for sensor: {doc['_id']}")
 
 def fetch_all_sensors():
-    # return "hello"
-    sensor_info = db.sensor.find({}, {"id": 1, "_id": 0})
-    print(sensor_info)
-    return
+    log.info("Fetching all sensors...")
+    sensor_info = list(db.sensor.find({}, {"id": 1, "_id": 0}))
+    log.info(f"Total sensors fetched: {len(sensor_info)}")
+
     with ThreadPoolExecutor(max_workers=10) as executor:
         futures = {executor.submit(fetch_sensor_data, doc["id"]): doc["id"] for doc in sensor_info}
         for future in as_completed(futures):
             try:
                 future.result()
-                print(f"Fetched data for sensor: {futures[future]}")
+                log.info(f"Fetched data for sensor: {futures[future]}")
             except Exception as e:
-                print(f"Error fetching data for sensor {futures[future]}: {e}")
+                log.error(f"Error fetching data for sensor {futures[future]}: {e}")
 
 with DAG(
         dag_id="ETL",
@@ -53,5 +59,5 @@ with DAG(
         task_id="fetch_all_sensors",
         python_callable=fetch_all_sensors,
     )
-#
-    fetch_sensors_task
+
+fetch_sensors_task
