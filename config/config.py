49c356004d361313a2437747f36f7478c860f02d
bhattshubham1401 <shubhambhatt687@gmail.com>
2025-03-25 15:31:16 +0530

    add

--- config/config.py
+++ config/config.py
@@ -1,36 +1,14 @@
 from pymongo import MongoClient
-<<<<<<< HEAD
-import os
-# from logs import logs_config
-
-client = MongoClient("mongodb://35.154.221.2:27017/")
-print("Database connection successful")
-
-
-def get_database():
-    try:
-
-        database = client.pvvnl
-        return database
-    except Exception as e:
-        # logs_config.logger.error("Database connection failed:", exc_info=True)
-        raise e
-=======
 from dotenv import load_dotenv
 import os
-from logs import logs_config
-
+# from logs import logs_config
 load_dotenv()
-
 client = MongoClient("mongodb://13.127.57.185:27017/")
-logs_config.logger.info("Database connection successful")
+# logs_config.logger.info("Database connection successful")
 def get_database():
     try:
         database = client.pvvnl 
         return database
     except Exception as e:
-        logs_config.logger.error("Database connection failed:", exc_info=True)
+        # logs_config.logger.error("Database connection failed:", exc_info=True)
         raise e
-        
->>>>>>> 9b96abbeb2c5f32bd3020b6ddfa7456fb862d75c
-
